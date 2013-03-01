9ab37450e6308237fe2691b5c3b29271b41fb729
Michael Tharp <gxti@partiallystapled.com>
2013-02-28 21:45:51 -0500

    In-progress refactor of depot_downloader

--- pysteamkit/depot_downloader.py
+++ pysteamkit/depot_downloader.py
@@ -1,7 +1,5 @@
-from gevent import monkey
-monkey.patch_all()
-
 import argparse
+import logging
 import os
 import time
 from getpass import getpass
@@ -15,23 +13,10 @@ from pysteamkit.steam3.cdn_client import CDNClient
 from pysteamkit.steam3.client import SteamClient
 from pysteamkit.util import Util
 
-parser = argparse.ArgumentParser(description='DepotDownloader downloads depots.')
-parser.add_argument('appid', type=int, help='AppID to download')
-parser.add_argument('--branch', type=str, default='public', help='Application branch to download')
-parser.add_argument('--dir', type=str, default='downloads/', help='Directory to operate within')
-parser.add_argument('--depots', type=int, nargs='*', help='Specific depots to download')
-parser.add_argument('--username', type=str, help='Username to sign in with')
-parser.add_argument('--password', type=str, help='Account password')
-parser.add_argument('--cellid', type=int, help='Cell ID to use for downloads')
-parser.add_argument('--verify-all', action='store_true', default=False, help='Specify to verify all files')
+log = logging.getLogger('dd')
 
-args = parser.parse_args()
-client = None
-steamapps = None
-content_client_pool = None
-install_manifest = None
 
-class SteamClientHandler:
+class SteamClientHandler(object):
 	def get_sentry_file(self, username):
 		filename = 'sentry_%s.bin' % (username,)
 		if not os.path.exists(filename):
@@ -49,51 +34,122 @@ class SteamClientHandler:
 		emsg = Util.get_msg(emsg)
 
 
-def get_depots_for_app(appid, filter):
-	if not 'depots' in steamapps.app_cache[appid]:
-		return []
-	return [int(key) for key,values in steamapps.app_cache[appid]['depots'].iteritems() if key.isdigit() and (not filter or int(key) in filter)]
-	
-def get_depot(appid, depotid):
-	return steamapps.app_cache[appid]['depots'][str(depotid)]
+class DownloaderError(RuntimeError):
+	pass
 
-def get_depot_key(appid, depotid):
-	depot_key_result = steamapps.get_depot_key(depotid, args.appid)
-	if depot_key_result.eresult != EResult.OK:
-		return (depotid, None)
 
-	return (depotid, depot_key_result.depot_encryption_key)
-	
-def get_depot_manifest(depotid, manifestid):
-	client = content_client_pool.get_client(depotid)
-	(status, manifest) = client.download_depot_manifest(depotid, manifestid)
-	if manifest:
-		content_client_pool.return_client(client)
-		return (depotid, manifest, status)
-	return (depotid, None, status)
-	
-def get_depot_chunkstar(args):
-	return get_depot_chunk(*args)
-	
-def get_depot_chunk(depotid, chunk):
-	client = content_client_pool.get_client(depotid)
-	(status, chunk_data) = client.download_depot_chunk(depotid, chunk.sha.encode('hex'))
-	if chunk_data:
-		content_client_pool.return_client(client)
-		return (chunk, chunk.offset, chunk_data, status)
-	return (chunk, None, None, status)
-	
-def main(args):
-	global client, steamapps, content_client_pool
-	
-	install_manifest = DepotManifest()
-	if os.path.exists('install.manifest'):
-		try:
-			with open('install.manifest', 'rb') as f:
-				install_manifest.parse(f.read())
-		except:
-			os.remove('install.manifest')
+class DepotDownloader(object):
+	def __init__(self, client):
+		self.client = client
+		self.steamapps = client.steamapps
+		self.sessiontoken = client.get_session_token()
+		self.appid = None
+		self.depots = None
+		self.depot_keys = None
+		self.manifest_ids = None
 
+	def get_depots_for_app(self, appid, filter):
+		if not 'depots' in self.steamapps.app_cache[appid]:
+			return []
+		return [int(key) for key,values in self.steamapps.app_cache[appid]['depots'].iteritems() if key.isdigit() and (not filter or int(key) in filter)]
+		
+	def get_depot(self, appid, depotid):
+		return self.steamapps.app_cache[appid]['depots'][str(depotid)]
+
+	def get_depot_key(self, appid, depotid):
+		depot_key_result = self.steamapps.get_depot_key(depotid, appid)
+		if depot_key_result.eresult != EResult.OK:
+			return (depotid, None)
+
+		return (depotid, depot_key_result.depot_encryption_key)
+		
+	def get_depot_manifest(self, depotid, manifestid):
+		client = self.ccpool.get_client(depotid)
+		(status, manifest) = client.download_depot_manifest(depotid, manifestid)
+		if manifest:
+			self.ccpool.return_client(client)
+			return (depotid, manifest, status)
+		return (depotid, None, status)
+		
+	def get_depot_chunkstar(self, args):
+		return self.get_depot_chunk(*args)
+		
+	def get_depot_chunk(self, depotid, chunk):
+		client = self.ccpool.get_client(depotid)
+		(status, chunk_data) = client.download_depot_chunk(depotid, chunk.sha.encode('hex'))
+		if chunk_data:
+			self.ccpool.return_client(client)
+			return (chunk, chunk.offset, chunk_data, status)
+		return (chunk, None, None, status)
+
+	def set_appid(self, appid):
+		licenses = self.steamapps.get_licenses()
+		licenses = [x.package_id for x in licenses] if licenses else [17906]
+		print("Licenses: %s" % (licenses,))
+		
+		product_info = self.steamapps.get_product_info(apps=[appid],
+				packages=licenses)
+		needs_token = [x.appid for x in product_info.apps if x.missing_token]
+		
+		if needs_token:
+			tokens = self.steamapps.get_access_tokens(needs_token)
+			if not tokens.app_access_tokens:
+				raise DownloaderError(
+						"Unable to get an access token for app %d" % (appid,))
+			access_token = tokens.app_access_tokens[0].access_token
+			product_info = self.steamapps.get_product_info(
+					apps=[(appid,access_token)], packages=licenses)
+
+		valid_apps = [x.appid for x in product_info.apps]
+		if appid not in valid_apps:
+			raise DownloaderError("Could not find an app for id %d" % (appid,))
+		if not self.steamapps.has_license_for_app(appid):
+			raise DownloaderError("You do not have a license for app %d"
+					% (appid,))
+		self.appid = appid
+
+	def set_depots(self, depot_filter=(), branch='public'):
+		assert self.appid
+		depots = self.get_depots_for_app(self.appid, depot_filter)
+		if not depots:
+			raise DownloaderError("No depots available for app %d "
+					"given filter %s" % (self.appid, depot_filter))
+
+		manifest_ids = {}
+		for depotid in depots:
+			depot = self.get_depot(self.appid, depotid)
+			log.info('Depot %d: "%s"', depotid, depot['name'])
+			manifests = depot.get('manifests')
+			encrypted_manifests = depot.get('encrypted_manifests')
+			if manifests and manifests.get(branch):
+				manifest = manifests[branch]
+			elif encrypted_manifests and encrypted_manifests.get(branch):
+				# FIXME
+				assert False
+			else:
+				# FIXME
+				assert False
+			manifest_ids[depotid] = manifest
+
+		log.info("Fetching decryption keys")
+		pool = Pool(4)
+		key_fetch = [pool.spawn(self.get_depot_key, self.appid, depotid)
+				for (depotid, manifest) in manifest_ids.iteritems()]
+		pool.join()
+		
+		depot_keys = {}
+		for job in key_fetch:
+			(depotid, depot_key) = job.value
+			if depot_key is None:
+				raise DownloaderError("Could not get depot key for depot %d"
+						% (depotid,))
+			depot_keys[depotid] = depot_key
+
+		self.manifest_ids = manifest_ids
+		self.depot_keys = depot_keys
+
+
+def signin(args):
 	while args.username and not args.password:
 		args.password = getpass('Please enter the password for "' + args.username + '": ')
 	
@@ -121,87 +177,40 @@ def main(args):
 		return
 
 	print("Signed into Steam3 as %s" % (str(client.steamid),))
-	
 	if args.cellid == None:
 		print("No cell id specified, using Steam3 specified: %d" % (logon_result.cell_id,))
 		args.cellid = logon_result.cell_id
-		
-	sessiontoken = client.get_session_token()
-	
-	steamapps = client.steamapps
-	
-	licenses = steamapps.get_licenses()
-	licenses = [x.package_id for x in licenses] if licenses else [17906]
-	print("Licenses: %s" % (licenses,))
-	
-	product_info = steamapps.get_product_info(apps = [args.appid], packages = licenses)
-	needs_token = [x.appid for x in product_info.apps if x.missing_token]
-	
-	if len(needs_token) > 0:
-		tokens = steamapps.get_access_tokens(needs_token)
-		if len(tokens.app_access_tokens) == 0:
-			print("Unable to get an access token for app %d" % (args.appid,))
-			return
-			
-		access_token = tokens.app_access_tokens[0].access_token
-		product_info = steamapps.get_product_info(apps = [(args.appid,access_token)], packages = licenses)
-	
-	valid_apps = [x.appid for x in product_info.apps]
-	valid_packages = [x.packageid for x in product_info.packages]
+	return client
 
-	if not args.appid in valid_apps:
-		print("Could not find an app for id %d" % (args.appid,))
-		return
 
-	if not steamapps.has_license_for_app(args.appid):
-		print("You do not have a license for app %d" % (args.appid,))
-		return
+def main():
+	parser = argparse.ArgumentParser(description='DepotDownloader downloads depots.')
+	parser.add_argument('appid', type=int, help='AppID to download')
+	parser.add_argument('--branch', type=str, default='public', help='Application branch to download')
+	parser.add_argument('--dir', type=str, default='downloads/', help='Directory to operate within')
+	parser.add_argument('--depots', type=int, nargs='*', help='Specific depots to download')
+	parser.add_argument('--username', type=str, help='Username to sign in with')
+	parser.add_argument('--password', type=str, help='Account password')
+	parser.add_argument('--cellid', type=int, help='Cell ID to use for downloads')
+	parser.add_argument('--verify-all', action='store_true', default=False, help='Specify to verify all files')
+	args = parser.parse_args()
 
-	#TODO
-	app_ticket = None
+	logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s',
+			datefmt='%T')
 
-	depots = get_depots_for_app(args.appid, args.depots)
-	
-	if len(depots) == 0:
-		print("No depots available for app %d given filter %s" % (args.appid, args.depots))
-		return
-	
-	depot_keys = dict()
-	depot_manifest_ids = []
-	
-	print("Depots found:")
-	for depotid in depots:
-		depot = get_depot(args.appid, depotid)
-		print("* %d %s" % (depotid, depot['name']))
-				
-		manifests = depot.get('manifests')
-		encrypted_manifests = depot.get('encrypted_manifests')
-		
-		if manifests and manifests.get(args.branch):
-			manifest = manifests[args.branch]
-		elif encrypted_manifests and encrypted_manifests.get(args.branch):
-			encrypted_gid = encrypted_manifests[args.branch]
+	install_manifest = DepotManifest()
+	if os.path.exists('install.manifest'):
+		try:
+			with open('install.manifest', 'rb') as f:
+				install_manifest.parse(f.read())
+		except:
+			os.remove('install.manifest')
 
-		depot_manifest_ids.append((depotid, manifest))
+	client = signin(args)
+	dl = DepotDownloader(client)
+	dl.set_appid(args.appid)
+	dl.set_depots(args.depots)
 
-	if len(depot_manifest_ids) == 0:
-		print("Unable to find any downloadable depots for app %d branch %s" % (args.appid,args.branch))
-		return
-		
-	print("Total %d manifests" % (len(depot_manifest_ids),))
-	
-	print("Fetching decryption keys")
-	pool = Pool(4)
-	key_fetch = [pool.spawn(get_depot_key, args.appid, depotid) for (depotid, manifest) in depot_manifest_ids]
-	pool.join()
-	
-	for job in key_fetch:
-		(depotid, depot_key) = job.value
-		if depot_key == None:
-			print("Could not get depot key for %d" % (depotid,))
-			return
-		depot_keys[depotid] = depot_key
-		
 	print("Building CDN server list")
 	base_server_list = client.server_list[EServerType.CS]
 	
@@ -220,17 +229,21 @@ def main(args):
 		
 	print("Found %d content servers" % (len(content_servers),))
 	
-	content_client_pool = CDNClientPool(content_servers, app_ticket, client.steamid)
+	#TODO
+	app_ticket = None
+	dl.ccpool = CDNClientPool(content_servers, app_ticket, client.steamid)
 	
 	print("Downloading depot manifests")
 	depot_manifests_retrieved = []
 	depot_manifests = []
 	num_tries = 0
 	
-	while len(depot_manifests_retrieved) < len(depot_manifest_ids) and num_tries < 4:
+	while len(depot_manifests_retrieved) < len(dl.manifest_ids) and num_tries < 4:
 		num_tries += 1
 		pool = Pool(4)
-		manifest_fetch = [pool.spawn(get_depot_manifest, depotid, manifestid) for (depotid, manifestid) in depot_manifest_ids if not depotid in depot_manifests_retrieved]
+		manifest_fetch = [pool.spawn(dl.get_depot_manifest, depotid, manifestid)
+				for (depotid, manifestid) in dl.manifest_ids.iteritems()
+				if depotid not in depot_manifests_retrieved]
 		pool.join()
 		
 		for job in manifest_fetch:
@@ -242,7 +255,7 @@ def main(args):
 				depot_manifest = DepotManifest()
 				depot_manifest.parse(manifest)
 				
-				if not depot_manifest.decrypt_filenames(depot_keys[depotid]):
+				if not depot_manifest.decrypt_filenames(dl.depot_keys[depotid]):
 					print("Could not decrypt depot manifest for %d" % (depotid,))
 					return
 					
@@ -262,7 +275,7 @@ def main(args):
 	Util.makedir(path_prefix)
 
 	for (depotid, manifest) in depot_manifests:
-		depot = get_depot(args.appid, depotid)
+		depot = dl.get_depot(args.appid, depotid)
 		depot_files = []
 
 		files_changed = install_manifest.get_files_changed(manifest)
@@ -323,7 +336,7 @@ def main(args):
 		
 	pool = Pool(4)
 	for (depotid, depot_files) in depot_download_list:
-		depot = get_depot(args.appid, depotid)
+		depot = dl.get_depot(args.appid, depotid)
 		print("Downloading \"%s\"" % (depot['name'],))
 
 		last_write = 0
@@ -343,12 +356,12 @@ def main(args):
 				while len(chunks_completed) < len(chunks):
 					downloads = [(depotid, chunk) for chunk in chunks if not chunk.offset in chunks_completed]
 					
-					for (chunk, offset, chunk_data, status) in pool.imap(get_depot_chunkstar, downloads):
+					for (chunk, offset, chunk_data, status) in pool.imap(dl.get_depot_chunkstar, downloads):
 						if status != 200:
 							print("Chunk failed %s %d" % (chunk.sha.encode('hex'),status))
 							continue
 							
-						chunk_data = CDNClient.process_chunk(chunk_data, depot_keys[depotid])
+						chunk_data = CDNClient.process_chunk(chunk_data, dl.depot_keys[depotid])
 						f.seek(offset)
 						f.write(chunk_data)
 						total_bytes_downloaded += len(chunk_data)
@@ -368,4 +381,4 @@ def main(args):
 		
 	print("[%s/%s] Completed" % (Util.sizeof_fmt(total_bytes_downloaded), Util.sizeof_fmt(total_download_size)))
 
-main(args)
+main()
